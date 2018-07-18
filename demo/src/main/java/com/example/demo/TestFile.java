bbec49b1ce13944f414efd589ab1a8216b1a9057
sxc <466853319@qq.com>
2018-07-18 10:59:26 +0800

    测试修改

--- demo/src/main/java/com/example/demo/TestFile.java
+++ demo/src/main/java/com/example/demo/TestFile.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 public class TestFile {
 
-    public static void main(String[] args) {
-        System.out.println("输出检车");
-    }
+//    public static void main(String[] args) {
+//        System.out.println("输出检车");
+//    }
 }
