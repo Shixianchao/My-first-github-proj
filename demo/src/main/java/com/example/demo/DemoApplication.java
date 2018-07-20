b612a60d04cad84833fcae59f020b15f870a6cab
sxc <466853319@qq.com>
2018-07-20 10:17:42 +0800

    吧vv

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -19,16 +19,7 @@ public class DemoApplication {
         System.out.println("我已经改了一个东西");
         System.out.println("我已经改了第二个东西");
         System.out.println("我已经改了第三个东西");
-        System.out.println("你吗，我现在是在分支一上面更改的东西");
-        System.out.println("我在分支三上更改的东西，并且更改完成之后提交到分支1上面的");
-        System.out.println("第一次commit");
-        System.out.println("第二次commit");
-        System.out.println("分之一第二次commit");
-        System.out.println("分支4上面改的东西");
-        System.out.println("分之一上面改东西第一次提交");
-        System.out.println("分之一上面改东西第二次提交");
-        System.out.println("分之一上面改东西第三次提交");
-        System.out.println("分支一三次提交之后的stash操作");
+        System.out.println("master分支测试");
         return "123123123";
     }
 
